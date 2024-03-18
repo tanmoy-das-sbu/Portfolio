4710bbc28db66830fff49983994b904c2874f562
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-18 20:32:25 +0530

    created updateById api.

---
+++ backend/src/middlewares/updateSchedule.middleware.js
@@ -0,0 +1,20 @@
+import Schedule from '../models/schedule.model.js';
+
+const updateScheduleByIdMiddleware = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { date, tasks } = req.body;
+
+        const updatedSchedule = await Schedule.findByIdAndUpdate(id, { date, tasks }, { new: true });
+
+        if (!updatedSchedule) {
+            return res.status(404).json({ message: 'Schedule not found' });
+        }
+
+        res.status(200).json(updatedSchedule);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export default updateScheduleByIdMiddleware;
--- backend/src/routes/schedule.route.js
+++ backend/src/routes/schedule.route.js
@@ -3,14 +3,16 @@ const router = express.Router();
 import getScheduleByDate from "../middlewares/getScheduleByDate.middleware.js";
 import addScheduleMiddleware from '../middlewares/addSchedule.middleware.js';
 import getAllSchedule from '../middlewares/getAllSchedule.middleware.js';
+import updateScheduleByIdMiddleware from '../middlewares/updateSchedule.middleware.js';
 
 router.get('/:date', getScheduleByDate, (req, res) => {
   res.json(res.schedule);
-  
 });
 
 router.post('/Schedule/Add' , addScheduleMiddleware);
 
 router.get('/Schedule/GetAll', getAllSchedule);
+
+router.put('/Schedule/UpdateById/:id', updateScheduleByIdMiddleware);
 
 export default router;
