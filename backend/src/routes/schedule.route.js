36dbbbf90ed6125f29cf587686c79266c02f3140
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-18 20:03:25 +0530

    created getAll api

--- backend/src/routes/schedule.route.js
+++ backend/src/routes/schedule.route.js
@@ -2,12 +2,15 @@ import express from 'express';
 const router = express.Router();
 import getScheduleByDate from "../middlewares/getScheduleByDate.middleware.js";
 import addScheduleMiddleware from '../middlewares/addSchedule.middleware.js';
+import getAllSchedule from '../middlewares/getAllSchedule.middleware.js';
 
 router.get('/:date', getScheduleByDate, (req, res) => {
   res.json(res.schedule);
   
 });
 
-router.post('/Schedule/Add' , addScheduleMiddleware)
+router.post('/Schedule/Add' , addScheduleMiddleware);
+
+router.get('/Schedule/GetAll', getAllSchedule);
 
 export default router;
