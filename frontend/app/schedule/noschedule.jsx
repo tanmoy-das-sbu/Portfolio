f98fda198ef0c2bd35d723413f369feb5f201573
Paratparshahi <95007617+Paratparshahi@users.noreply.github.com>
2024-03-20 14:14:23 +0530

    Updated

--- frontend/app/schedule/noschedule.jsx
+++ frontend/app/schedule/noschedule.jsx
@@ -1,7 +1,7 @@
 const NoSchedule = () => {
     return (
         <div className="container m-auto">
-            <h2 className="font-bold text-3xl text-center mb-5">No Schedule for Tomorrow</h2>
+            <h2 className="font-bold text-3xl text-center mb-5">No Schedule available</h2>
             {/* Add any additional content or styling as needed */}
         </div>
     )
