36b545dfca77ccb9a13e75972425b14ca3afe9ad
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-20 21:19:42 +0530

    added img and replaced date with startDate and endDate.

--- backend/src/models/schedule.model.js
+++ backend/src/models/schedule.model.js
@@ -17,22 +17,32 @@ const taskSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    img: {
+        type: String,
+        required: false
+    },
     visibility: {
         type: String,
-        enum: ['public', 'private'], 
+        enum: ['public', 'private'],
         default: 'public'
     }
 });
 
 const scheduleSchema = new mongoose.Schema({
-    date: {
+    startDate: {
         type: Date,
         required: true,
-        index: true
-        
+        index: true,
+        unique: true
     },
-    tasks: [taskSchema] 
-},{
+    endDate: {
+        type: Date,
+        required: false,
+        index: true,
+        unique: false
+    },
+    tasks: [taskSchema]
+}, {
     versionKey: false
 });
 
