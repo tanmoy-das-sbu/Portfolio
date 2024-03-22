b92498d5ad75a291f9b65fcaae2a6954d5e3ad29
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2024-03-22 09:40:37 -0700

    added get by id api.

---
+++ backend/src/Controllers/getByIdSchedule.middleware.js
@@ -0,0 +1,22 @@
+import Schedule from '../models/schedule.model.js';
+
+const getByIdSchedule = async (req, res) => {
+    try {
+        const data = await Schedule.findById({ _id: req.params._id });
+        if (data) {
+            res.status(200).json({
+                message: 'success',
+                data
+            });
+        } else {
+            res.status(500).json({
+                message: 'no data present'
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export default getByIdSchedule;
