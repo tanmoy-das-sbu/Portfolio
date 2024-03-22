7de24e8f7942e9a3729d6250704254f969d55f77
Harshit77Nav <harshit77navneet@gmail.com>
2024-03-22 11:16:45 +0530

    status code change for  getschedulebyid

--- backend/src/Controllers/getScheduleByDate.middleware.js
+++ backend/src/Controllers/getScheduleByDate.middleware.js
@@ -9,7 +9,7 @@ async function getScheduleByDate(req, res, next) {
     const schedule = await Schedule.findOne({ startDate: { $eq: queryDate } });
 
     if (!schedule) {
-      return res.status(404).json({ message: 'Schedule not found' });
+      return res.status(204).json({ message: 'Schedule not found' });
     }
 
     res.schedule = schedule;
