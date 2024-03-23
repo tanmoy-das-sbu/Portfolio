2ce83be827f183f24af629c7fe33a64c079b93bd
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-23 14:54:25 +0530

    Update nav.jsx

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -12,10 +12,10 @@ const Nav = () => {
         <div className="w-max-md h-fit bg-[#ffbe5d] p-3 sticky top-0 z-30">
             <div className="container flex flex-row justify-around" >
                 <div className="">
-                    <h2 className="font-bold md:text-4xl text-lg text-white" >{name}</h2>   
-                    <p className="font-bold md:text-3xl text-md text-white">Member-Elect, Rajya Sabha</p>
-                    <p className="font-bold md:text-3xl text-md text-white">General Secretary, BJP</p>
-                    <p className="font-bold md:text-3xl text-md text-white">Jharkhand</p>
+                    <u className="text-white"><h2 className="font-bold md:text-4xl text-xl text-white" >{name}</h2></u>    
+                    <p className="font-bold md:text-3lg text-lg text-white">Member-Elect, Rajya Sabha</p>
+                    <p className="font-bold md:text-3lg text-lg text-white">General Secretary, BJP</p>
+                    <p className="font-bold md:text-3lg text-lg text-white">Jharkhand</p>
                 </div>
                 <div className="flex items-center bg-white rounded-full p-3 ">
                     <Image  src={Logo} alt="slide_image" className="w-[80px] h-[80px] rounded-full  border-xl-white-600 " />
