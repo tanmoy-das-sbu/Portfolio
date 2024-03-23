7597e3fbde39148aa3f3c0f0b146e6995b6bc25a
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-23 15:06:16 +0530

    Update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "../src/components/header";
+import Header from "../src/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav/>
+        <Header/>
         {children}
       </body>
     </html>
