c645778f0384c0ced7673aaa98507e0819854848
Paratparshahi <95007617+Paratparshahi@users.noreply.github.com>
2024-03-23 11:20:53 +0530

    Applied hemburger icon and made it responsive

--- frontend/src/components/navigationBar.jsx
+++ frontend/src/components/navigationBar.jsx
@@ -2,7 +2,8 @@
 
 import React, { useState } from 'react';
 import './navigationBar.css';
-
+import { Cross1Icon, HamburgerMenuIcon } from '@radix-ui/react-icons';
+Cross1Icon
 const NavigationMenuDemo = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -11,22 +12,35 @@ const NavigationMenuDemo = () => {
     };
 
     return (
-        <div className='navigation'>
-            <div className='mobileMenuToggle' onClick={toggleMobileMenu}>
-                <div className='bar'></div>
-                <div className='bar'></div>
-                <div className='bar'></div>
-            </div>
-            <ul className={`menu ${isMobileMenuOpen ? 'show' : ''}`}>
-                <li><p>Home</p></li>
-                <li><span className="separator">|</span></li>
-                <li><p>Events</p></li>
-                <li><span className="separator">|</span></li>
-                <li><p>About</p></li>
-                <li><span className="separator">|</span></li>
-                <li><p>Contact</p></li>
-            </ul>
-        </div>
+        <nav class="nav">
+  <a href="#" class="nav__logo"></a>
+
+ 
+  <div class="nav__menucontainer">
+    <div class="nav__listcontainer" tabindex="0">
+      <ul class="nav__menu" id="navmenu">
+        <li class="nav__item">
+          <a href="#" class="nav__link">About</a>
+        </li>
+        <li class="nav__item">
+          <a href="#" class="nav__link">Projects</a>
+        </li>
+        <li class="nav__item">
+          <a href="#" class="nav__link">Contact</a>
+        </li>
+        <li class="nav__item">
+          <a href="#" class="nav__link">Blog</a>
+        </li>
+      </ul>
+      <a id="hamburger" href="#navmenu" title="menu" class="nav__hamburger">
+      <HamburgerMenuIcon  height={'22px'} width={'22px'}/>
+      </a>
+    </div>
+    <a href="#!" title="close menu" class="nav__hamburgerclose"><Cross1Icon/></a>
+  </div>
+
+</nav>
+
     );
 }
 
