98ba1f2aa7411049042dc41a7176bc7854a6da61
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-23 10:25:00 +0530

    Nav - WIP

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -6,19 +6,19 @@ import NavigationMenuUI from './navigationBar';
 
 const Nav = () => {
     const name = 'Dr. PRADEEP VARMA';
-    const detail = `Member-Elect, Rajya Sabha
-    General Secretary, BJP
-    Jharkhand`
+
     return (
     <>
         <div className="nav-div h-fit sticky top-0 z-30">
-            <div className="container flex flex-row justify-between" style={{ height: '4em' }}>
-                <div className="flex flex-col items-center nav-in-div">
-                    <h2 className="font-bold md:text-4xl text-xl text-white" >{name}</h2>
-                    {/* <h5>{detail}</h5> */}
+            <div className="container flex flex-row justify-around" style={{ height: '4em' }}>
+                <div className="nav-in-div">
+                    <u className="text-white"><h2 className="font-bold md:text-4xl text-xl text-white" >{name}</h2></u>   
+                    <p className="font-bold md:text-4base text-base text-white">Member-Elect, Rajya Sabha</p>
+                    <p className="font-bold md:text-4base text-base text-white">General Secretary, BJP</p>
+                    <p className="font-bold md:text-4base text-base text-white">Jharkhand</p>
                 </div>
                 <div className="flex items-center">
-                    {/* <Image src={Logo} alt="slide_image" className="logo h-auto " /> */}
+                    <Image src={Logo} alt="slide_image" className="logo h-auto " />
                 </div>
             </div>
         </div>
