4539bb6d71cb11522332ed2aab22f9e920d58b1f
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-23 15:57:04 +0530

    updated

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../src/components/header";
+import Footer from "../src/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
