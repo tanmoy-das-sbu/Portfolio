8c523ac6686a56493784fd8724da9aa1d26dcdbd
Harshit77Nav <harshit77navneet@gmail.com>
2024-03-27 15:49:58 +0530

    updated getScheduleByDate service

--- backend/src/Controllers/getScheduleByDate.middleware.js
+++ backend/src/Controllers/getScheduleByDate.middleware.js
@@ -5,15 +5,30 @@ async function getScheduleByDate(req, res, next) {
   try {
 
     const queryDate = new Date(startDate);
+    const currentDate = new Date();
+    currentDate.setUTCHours(0, 0, 0, 0);
+    if (queryDate.getTime() === currentDate.getTime()) {
+      const schedule = await Schedule.find({ startDate: { $eq: queryDate } });
+      const ongoingEvents = await Schedule.find({
+        startDate: { $lte: currentDate },
+        endDate: { $gte: currentDate }
+      });
+      const eventArray = [...schedule, ...ongoingEvents];
+      if (!eventArray) {
+        return res.status(204).json({ message: 'Schedule not found' });
+      }
 
-    const schedule = await Schedule.find({ startDate: { $eq: queryDate } });
+      res.schedule = eventArray;
+      next();
+    } else {
+      const schedule = await Schedule.find({ startDate: { $eq: queryDate } });
+      if (!schedule) {
+        return res.status(204).json({ message: 'Schedule not found' });
+      }
 
-    if (!schedule) {
-      return res.status(204).json({ message: 'Schedule not found' });
+      res.schedule = schedule;
+      next();
     }
-
-    res.schedule = schedule;
-    next();
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
