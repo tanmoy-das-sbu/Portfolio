972d0b6271ca4528c1a29aab5ca1804cc4722079
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-29 17:07:06 +0530

    updated footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,4 +1,3 @@
-"use-client"
 import './footer.css';
 import { Input } from '@/components/ui/input';
 
@@ -28,6 +27,9 @@ const Footer = () => {
                         <Input id="email" placeholder="Enter your Email" type="email" name="email" className="h-10" />
                         <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 h-10 text-sm flex justify-center items-center">Subscribe</button>
                     </div>
+                    <div className='social-div'>
+
+                    </div>
                 </section>
                 <section className="footer-section">
                     <h2 className='font-bold'>Contact</h2>
@@ -39,8 +41,8 @@ const Footer = () => {
                             <p>Email: <u><a href="mailto:example@gmail.com">example@gmail.com</a></u></p>
                             <p>Tel.- <u><a href="tel:1234567890">1234567890</a></u></p>
                         </div>
-                        <div>
-
+                        <div className='.social-div'>
+                           
                         </div>
                     </div>
                 </section>
