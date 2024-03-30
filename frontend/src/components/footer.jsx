5c387469f5a8bbad9101a103bc39959b8c9428de
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-03-30 14:25:06 +0530

    updated footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -12,10 +12,10 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip"
 import Image from 'next/image';
-import twitter from '../assets/icons/twitter.png';
-import instagram from '../assets/icons/instagram.png';
-import youtube from '../assets/icons/youtube.png';
-import facebook from '../assets/icons/facebook.png';
+import twitter from '../assets/icons/twitter.svg';
+import instagram from '../assets/icons/instagram.svg';
+import youtube from '../assets/icons/youtube.svg';
+import facebook from '../assets/icons/facebook.svg';
 
 
 const Footer = () => {
@@ -44,52 +44,52 @@ const Footer = () => {
                         <Input id="email" placeholder="Enter your Email" type="email" name="email" className="h-10" />
                         <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 h-10 text-sm flex justify-center items-center">Subscribe</button>
                     </div>
-                    
+
                     <div className='social-div'>
                         <h2 className='font-bold'>Socials</h2>
                         <hr />
                         <div className='flex justify-between items-center icons-div mt-2'>
-                        <TooltipProvider>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    {/* <FacebookRoundedIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-blue-500 social-icons"/> */}
-                                    <i><Image src={facebook} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"/></i>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Facebook</p>
-                                </TooltipContent>
-                            </Tooltip>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    {/* <XIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-black social-icons" /> */}
-                                    <i><Image src={twitter} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"/></i>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Twitter</p>
-                                </TooltipContent>
-                            </Tooltip>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    {/* <YouTubeIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-red-500 social-icons" /> */}
-                                    <i><Image src={youtube} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"/></i>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Youtube</p>
-                                </TooltipContent>
-                            </Tooltip>
-                            <Tooltip>
-                                <TooltipTrigger>
-                                    {/* <InstagramIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"  style={{ background: 'linear-gradient(115deg, #f9ce34, #ee2a7b, #6228d7)' }} /> */}
-                                    <i><Image src={instagram} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"/></i>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>Instagram</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        {/* <FacebookRoundedIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-blue-500 social-icons"/> */}
+                                        <i><Image src={facebook} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons" /></i>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Facebook</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        {/* <XIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-black social-icons" /> */}
+                                        <i><Image src={twitter} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons" /></i>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Twitter</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        {/* <YouTubeIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 text-red-500 social-icons" /> */}
+                                        <i><Image src={youtube} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons" /></i>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Youtube</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        {/* <InstagramIcon className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons"  style={{ background: 'linear-gradient(115deg, #f9ce34, #ee2a7b, #6228d7)' }} /> */}
+                                        <i><Image src={instagram} className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 social-icons" /></i>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Instagram</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        </div>
                     </div>
-                    </div>
-                    
+
                 </section>
                 <section className="footer-section">
                     <h2 className='font-bold'>Contact</h2>
