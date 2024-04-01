0160a771cdf45e2b458db17ebd0930f74fca9027
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-01 11:14:00 +0530

    Created Contact Schema

---
+++ backend/src/models/contact.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: false
+    },
+    message: {
+        type: String,
+        required: false
+    }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default Contact;
