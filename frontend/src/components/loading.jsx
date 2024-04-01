30e0beb135b8923a498c4fda894f131ccf17f532
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-01 14:25:28 +0530

    created Loading component.

---
+++ frontend/src/components/loading.jsx
@@ -0,0 +1,21 @@
+"use client";
+import "./loading.css"
+
+const Loading = () => {
+    return (
+        <div className="center">
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+        </div>
+    )
+}
+
+export default Loading;
