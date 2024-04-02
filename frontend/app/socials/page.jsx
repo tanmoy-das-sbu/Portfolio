09880b3f79c0405de670c310d6fef941404a810d
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-02 15:05:39 +0530

    Created Social Page UI

---
+++ frontend/app/socials/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+import "./page.css";
+import FacebookIcon from "../../src/assets/icons/facebook.svg";
+import InstagramIcon from "../../src/assets/icons/instagram.svg";
+import TwitterIcon from "../../src/assets/icons/twitter.svg";
+import YoutubeIcon from "../../src/assets/icons/youtube.svg";
+import Image from "next/image";
+
+const Socials = () => {
+    return (
+        <div className="pb-2">
+            <section className="social-heading h-60 flex justify-center items-center w-full">
+                <h1 className="text-4xl font-semibold">Social</h1>
+            </section>
+            <section className='container social-body container mx-auto mt-8'>
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+                    <div className="social-innerdiv">
+                        <div className="flex items-center gap-3 pb-6">
+                            <Image src={TwitterIcon} className="h-10 w-10 shadow-2xl rounded-full" />
+                            <h4 className="text-3xl font-semibold">Twitter</h4>
+                        </div>
+                        <div className="md:row-start-1 md:row-end-2">
+                            <a className="twitter-timeline" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/PKVarmaRanchi?ref_src=twsrc%5Etfw">Tweets by PKVarmaRanchi</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                        </div>
+                    </div>
+                    <div className="social-innerdiv">
+                        <div className="flex items-center gap-3 pb-6">
+                            <Image src={FacebookIcon} className="h-10 w-10 shadow-2xl rounded-full" />
+                            <h4 className="text-3xl font-semibold" style={{ color: "#097EEB" }}>Facebook</h4>
+                        </div>
+                        <div className="md:row-start-1 md:row-end-2">
+                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpkvarmaranchi&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="500px" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+                            </iframe>
+                        </div>
+                    </div>
+                    <div className="social-innerdiv">
+                        <div className="flex items-center gap-3 pb-6">
+                            <Image src={InstagramIcon} className="h-10 w-10 shadow-2xl rounded-full" />
+                            <h4 className="text-3xl font-semibold" style={{ color: "#E4405F" }}>Instagram</h4>
+                        </div>
+                        <div className="md:row-start-2 md:row-end-3">
+                            <iframe
+                                src="https://www.instagram.com/pkvarmaranchi/embed"
+                                width="500px"
+                                height="500px"
+                                frameBorder="0"
+                                scrolling="yes"
+                                allowtransparency="true">
+                            </iframe>
+                        </div>
+                    </div>
+                    <div className="social-innerdiv">
+                        <div className="flex items-center gap-3 pb-6">
+                            <Image src={YoutubeIcon} className="h-10 w-10 shadow-2xl rounded-full" />
+                            <h4 className="text-3xl font-semibold" style={{ color: "#BA202E" }}>Youtube</h4>
+                        </div>
+                        <div className="md:row-start-2 md:row-end-3">
+                            <iframe
+                                src="https://www.youtube.com/embed"
+                                width="500px"
+                                height="500px"
+                                frameBorder="0"
+                                scrolling="yes"
+                                allowtransparency="true"
+                                className="rounded-xl p-2.5 shadow-xl">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Socials;
