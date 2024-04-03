21f54b858b8c18944f8db7d1022fbb74082680e5
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-04-03 17:32:34 +0530

    Page update

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -22,33 +22,31 @@ function Home() {
               width="200"
             />
             <h2 className="mt-4 mb-2 text-xl font-semibold text-center">
-            Dr. Pradip Varma
+              Dr. Pradip Varma
             </h2>
             <p className="text-sm text-center text-gray-600">Varma in 2023</p>
             <p className="text-sm font-semibold text-center">
-              MEMBER OF PARLIAMENT RAJYA SABHA  (JHARKHAND) 
-              </p>
+              MEMBER OF PARLIAMENT RAJYA SABHA (JHARKHAND)
+            </p>
             <div className="mt-4 p-2 border-t border-gray-200">
-              
               <p className="text-sm font-semibold">
-              BJP Jharkhand State General Secretary
+                BJP Jharkhand State General Secretary
               </p>
               <p className="text-sm font-semibold">
-              Additionally headquarters -in-charge BJP Jharkhand 
+                Additionally headquarters -in-charge BJP Jharkhand
               </p>
               <p className="text-xs text-gray-600">Incumbent</p>
               <p className="text-xs text-gray-600">
-              13 December 2020 - present
+                13 December 2020 - present
               </p>
               <p className="text-sm font-semibold">
-              BJP Jharkhand State Vice President
+                BJP Jharkhand State Vice President
               </p>
               <p className="text-xs text-gray-600">Incumbent</p>
               <p className="text-xs text-gray-600">Assumed office</p>
               <p className="text-xs text-gray-600">
-              2 July 2016 – 11 December 2020
+                2 July 2016 – 11 December 2020
               </p>
-              
             </div>
           </div>
           <div className="lg:w-3/4 lg:pl-8">
@@ -62,14 +60,45 @@ function Home() {
               membership-in-charge, Khijri division of Ranchi.
             </p>
             <p className="text-justify mt-4">
-            Varma was the BJP's election in-charge for India's tribal state of Jharkhand, during the 2019 Lok Sabha elections. The BJP and its Coalition partner NDA won 12 out of 14 seats. As a result, Varma rose to state prominence and was appointed as the Party's State General Secretary in 2019.[4] He has played an organising and membership-promotional role in the elections of many states since 2019. In his initial two years, the BJP achieved success in state legislative assembly in Jharkhand where it won the Dhanbad , Nirsa , Baghmara and Sindri but lost ground in Jharia and tundi in 2019.Pradip Varma is also a 2nd year trained Swayamsevak from (RSS) Rashtriya Swayamsevak Sangh and also served as a Joint secretary in Seva Bharti (Prantiya toli ) at zonal level.
-
+              Varma was the BJP&apos;s election in-charge for India&apos;s
+              tribal state of Jharkhand, during the 2019 Lok Sabha elections.
+              The BJP and its Coalition partner NDA won 12 out of 14 seats. As a
+              result, Varma rose to state prominence and was appointed as the
+              Party&apos;s State General Secretary in 2019. He has played an
+              organising and membership-promotional role in the elections of
+              many states since 2019. In his initial two years, the BJP achieved
+              success in state legislative assembly in Jharkhand where it won
+              the Dhanbad , Nirsa , Baghmara and Sindri but lost ground in
+              Jharia and tundi in 2019.Pradip Varma is also a 2nd year trained
+              Swayamsevak from (RSS) Rashtriya Swayamsevak Sangh and also served
+              as a Joint secretary in Seva Bharti (Prantiya toli ) at zonal
+              level.
             </p>
             <p className="text-justify mt-4">
-            Varma was born on 24th February 1972 in Kolkata capital city of West Bengal into a Hindu Vaishya family but he is ancestral native of tehsil Barwadih in Latehar district of Jharkhand. Varma started his primary education at Subhas Vidya Mandir Muraripukur Kolkata. Later on for further studies he shifted to Sarvodaya School Muchipara in kolkata. From childhood he was interested in playing chess at the age of 12 and was a state level chess player.Varma also played cricket for the team Mohan Bagan at the age of 15.
+              Varma was born on 24th February 1972 in Kolkata capital city of
+              West Bengal into a Hindu Vaishya family but he is ancestral native
+              of tehsil Barwadih in Latehar district of Jharkhand. Varma started
+              his primary education at Subhas Vidya Mandir Muraripukur Kolkata.
+              Later on for further studies he shifted to Sarvodaya School
+              Muchipara in kolkata. From childhood he was interested in playing
+              chess at the age of 12 and was a state level chess player.Varma
+              also played cricket for the team Mohan Bagan at the age of 15.
             </p>
             <p className="text-justify mt-4">
-            He has done his intermediate in commerce stream in 1988 and undergraduate degree in commerce, B.Com from Seth Anand Ram Jaipuriya college of Calcutta University in 1991.He passed his executive MBA from Vinayaka Mission, and master’s degree of M.A (Hindi) from the same. Later he has done his completed his doctorate Phd. in Hindi language and literature. . His scholarly work has been on the topic of Ramcharitmanas : “Parivar aur Samaj prabadhan ke sutra” .He has also written blog on the several  biographies  on freedom fighters like Birsa Munda , Pt. Deen Dayal Upadhyaay and topics of national importance like  Hul Andolan, Vande matram etc. Apart from Hindi language, Varma is a polyglot person having full proficiency of English, Awadhi, Bangla ,Bhojpuri and 4 tribal languages of Jharkhand namely ,Nagpuri ,Mundari, Santhali ,and Kurmali.
+              He has done his intermediate in commerce stream in 1988 and
+              undergraduate degree in commerce, B.Com from Seth Anand Ram
+              Jaipuriya college of Calcutta University in 1991.He passed his
+              executive MBA from Vinayaka Mission, and master’s degree of M.A
+              (Hindi) from the same. Later he has done his completed his
+              doctorate Phd. in Hindi language and literature. . His scholarly
+              work has been on the topic of Ramcharitmanas : “Parivar aur Samaj
+              prabadhan ke sutra” .He has also written blog on the several
+              biographies on freedom fighters like Birsa Munda , Pt. Deen Dayal
+              Upadhyaay and topics of national importance like Hul Andolan,
+              Vande matram etc. Apart from Hindi language, Varma is a polyglot
+              person having full proficiency of English, Awadhi, Bangla
+              ,Bhojpuri and 4 tribal languages of Jharkhand namely ,Nagpuri
+              ,Mundari, Santhali ,and Kurmali.
             </p>
           </div>
         </div>
