3e62c692a6aa5908936e7b90a5dce8253d54aebc
Paratparshahi <95007617+Paratparshahi@users.noreply.github.com>
2024-04-04 12:50:06 +0530

    Update UpcomingSlider.jsx

--- frontend/app/schedule/UpcomingSlider/UpcomingSlider.jsx
+++ frontend/app/schedule/UpcomingSlider/UpcomingSlider.jsx
@@ -81,9 +81,9 @@ const UpcomingSlider = ({ data }) => {
     return (
         <section className="bg-[#FFFFFF]" >
             <hr />
-            <Carousel className="container relative z-20 noselect m-auto w-4/5 p-2 md:p-10"  >
-                <div className="text-6xl text-center">
-                    <h2 className="headingTag font-bold text-6xl md:text-4xl w-full text-center mb-5">Ongoing Events</h2>
+            <Carousel className="container relative z-20 noselect m-auto w-4/5 p-2 "  >
+                <div className=" text-center">
+                    <h2 className="headingTag font-bold sm:text-2xl md:text-4xl lg:text-5xl xs:text-xl w-full text-center text-2xl mb-5">Ongoing Events</h2>
                 </div>
                 <CarouselContent>
                     {ongoing && ongoing.map((event, index) => {
@@ -91,21 +91,19 @@ const UpcomingSlider = ({ data }) => {
                         return (
                             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                                 <CardContent className="flex aspect-square items-center justify-center p-1">
-                                    <Card className="w-[300px] flex flex-col justify-between">
+                                    <Card className="w-[300px] flex flex-col justify-between bg-cover bg-center"  >
                                         <CardHeader>
-                                            <div className="flex items-center space-x-2" style={{ margin: 'auto' }}>
-                                                <Avatar>
-                                                    <AvatarImage alt="" src="" />
-                                                    <AvatarFallback>CH</AvatarFallback>
-                                                </Avatar>
-                                                <div>
-                                                    <CardTitle>{event.heading}</CardTitle>
-
-                                                </div>
+                                            <div className="items-center text-center" >
+                                                <img src="https://4.bp.blogspot.com/-URiCjwBMxVc/TtzXRyJi90I/AAAAAAAADj8/MrZd0T5ewIg/s1600/bjp-flag.jpg" alt="" />
+                                                
                                             </div>
+                                            <div>
+                                                    
+                                        </div>
                                         </CardHeader>
                                         <CardContent className="text-center">
-                                            <div className="p-2 mb-4">
+                                            <div className="p-2 mb-1">
+                                            <CardTitle>{event.heading}</CardTitle>
                                                 <p className="text-sm">
                                                     Start Time: {event.startTime}
                                                 </p>
@@ -148,7 +146,7 @@ const UpcomingSlider = ({ data }) => {
                                                         </div>
                                                         <AlertDialogCancel>Close</AlertDialogCancel>
                                                         <AlertDialogAction>
-                                                            <Link className="p-5 bg-red-500 text-white" href={`/eventView/${event._id}`}> View More</Link>
+                                                            <Link className="p-5  text-white" href={`/eventView/${event._id}`}> View More</Link>
                                                         </AlertDialogAction>
 
                                                     </AlertDialogFooter>
@@ -167,8 +165,8 @@ const UpcomingSlider = ({ data }) => {
             </Carousel>
             <hr />
             <Carousel className="container relative z-20 noselect m-auto w-4/5 p-2 md:p-10"  >
-                <div className="text-6xl text-center">
-                    <h2 className="headingTag font-bold text-6xl md:text-4xl w-full text-center mb-5">Upcoming Events</h2>
+                <div className=" text-center">
+                    <h2 className="headingTag font-bold sm:text-2xl md:text-4xl lg:text-5xl xs:text-xl w-full text-center text-2xl mb-5">Upcoming Events</h2>
                 </div>
                 <CarouselContent>
                     {upcomingEvent && upcomingEvent.map((event, index) => {
@@ -178,18 +176,14 @@ const UpcomingSlider = ({ data }) => {
                                 <CardContent className="flex aspect-square items-center justify-center p-2">
                                     <Card className="w-[300px] flex flex-col justify-between">
                                         <CardHeader>
-                                            <div className="flex items-center space-x-1 " style={{ margin: 'auto' }}>
-                                                <Avatar>
-                                                    <AvatarImage alt="" src="" />
-                                                    <AvatarFallback>CH</AvatarFallback>
-                                                </Avatar>
-                                                <div>
-                                                    <CardTitle>{event.heading}</CardTitle>
-                                                </div>
+                                            <div className=" items-center space-x-1 text-center" style={{ margin: 'auto' }}>
+                                            <img src="https://4.bp.blogspot.com/-URiCjwBMxVc/TtzXRyJi90I/AAAAAAAADj8/MrZd0T5ewIg/s1600/bjp-flag.jpg" alt="" />
+                                            
                                             </div>
                                         </CardHeader>
                                         <CardContent className="text-center">
-                                            <div className="p-1 mb-4">
+                                            <div className="p-1 mb-1">
+                                            <CardTitle>{event.heading}</CardTitle>
                                                 <p className="text-lg mt-1">{new Date(event.startDate).toLocaleDateString()} - {new Date(event.startDate).toLocaleDateString()}</p>
                                                 <p className="text-sm">
                                                     Start Time: {event.startTime}
@@ -216,7 +210,7 @@ const UpcomingSlider = ({ data }) => {
                                                     <AlertDialogFooter>
                                                         <AlertDialogCancel>Close</AlertDialogCancel>
                                                         <AlertDialogAction>
-                                                            <Link className="p-5 bg-red-500 text-white" href={`/eventView/${event._id}`}> View More</Link>
+                                                            <Link className="p-5  text-white" href={`/eventView/${event._id}`}> View More</Link>
                                                         </AlertDialogAction>
                                                     </AlertDialogFooter>
                                                 </AlertDialogContent>
