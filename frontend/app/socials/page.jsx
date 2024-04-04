8beb987401e9412e4079c1cd3b6a452a05c19d54
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-04 15:28:02 +0530

    Update page.jsx

--- frontend/app/socials/page.jsx
+++ frontend/app/socials/page.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 const Socials = () => {
     return (
-        <div className="mt-[250px] pb-4">
+        <div className="mt-[270px] pb-4">
             <section className='container social-body mx-auto mt-8'>
                 <div className="grid md:grid-cols-2 grid-cols-1 gap-12">
                     <div className="social-innerdiv">
