6d5e3e178547bf77f9a7122d572c9d221e015aff
Harshit77Nav <harshit77navneet@gmail.com>
2024-04-05 16:47:07 +0530

    updated hero section carousel

--- frontend/app/schedule/page.jsx
+++ frontend/app/schedule/page.jsx
@@ -27,7 +27,6 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 import UpcomingSlider from "./UpcomingSlider/UpcomingSlider";
 import {
   AlertDialog,
@@ -180,7 +179,7 @@ const SecondSection = () => {
   }
 
   return (
-    <div className="mt-[300px]">
+    <div className="mt-[250px]">
       <div className="container  date-pic-div flex flex-row justify-center pt-4">
         <Popover>
           <PopoverTrigger asChild>
@@ -211,64 +210,77 @@ const SecondSection = () => {
             <NoScheduleForToday />
           ) : (
             <div>
-              <div className=" container  m-auto pt-4 md:pt-8">
+              <div className=" container  m-auto pt-4 md:pt-12">
                 {flaghead ? (
-                  <h2 className="headingTag font-bold text-6xl md:text-4xl w-full text-center mb-5">
+                  <h2 className="headingTag font-bold sm:text-2xl md:text-4xl xs:text-xl w-full text-center text-2xl mb-5">
                     Today&apos;s Events
                   </h2>
                 ) : (
-                  <h2 className="font-bold text-5xl md:text-4xl w-full text-center mb-5">
+                  <h2 className="headingTag font-bold sm:text-2xl md:text-4xl xs:text-xl w-full text-center text-2xl mb-5">
                     {date?.toDateString()} Schedule
                   </h2>
                 )}
-                <div className="container">
-                  <Swiper
-                    effect={"coverflow"}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    loop={true}
-                    slidesPerView={"auto"}
-                    autoplay={{
-                      delay: 10000,
-                      disableOnInteraction: false,
-                    }}
-                    coverflowEffect={{
-                      rotate: 0,
-                      stretch: 0,
-                      depth: 80,
-                      modifier: 2.5,
-                    }}
-                    pagination={{ el: ".swiper-pagination", clickable: true }}
-                    navigation={{
-                      nextEl: ".swiper-button-next",
-                      prevEl: ".swiper-button-prev",
-                      clickable: true,
-                    }}
-                    modules={[
-                      EffectCoverflow,
-                      Pagination,
-                      Navigation,
-                      Autoplay,
-                    ]}
-                    className="swiper_container"
-                  >
-                    {todaySchedule?.map((item, index) => (
-                      <SwiperSlide className="" key={index}>
-                        <div className="w-full flex items-center justify-center pt-8px">
-                          <div
-                            className="max-w-sm rounded-xl overflow-hidden text-center w-full"
-                            style={{
-                              boxShadow:
-                                "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
-                              backgroundColor: "#f7f7f7",
-                            }}
-                          >
+
+                <Swiper
+                  effect={"coverflow"}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  loop={true}
+                  slidesPerView={'auto'}
+                  autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                  }}
+                  coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 80,
+                    modifier: 2.5,
+                  }}
+                  pagination={{  clickable: true }}
+                  navigation={{
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                    clickable: true,
+                  }}
+                  modules={[
+                    EffectCoverflow,
+                    Pagination,
+                    Navigation,
+                    Autoplay,
+                  ]}
+                >
+                  {todaySchedule?.map((item, index) => (
+                    <SwiperSlide className="" key={index} >
+                        <div className="flex flex-col overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+                          <figure className="flex-1">
                             <img
-                              className="w-full"
-                              src="https://tse1.mm.bing.net/th?id=OIP.stuO9HtrREb2xPI9Tlu0LgHaHr&pid=Api&rs=1&c=1&qlt=95&w=119&h=124"
-                              alt="Sunset in the mountains"
+                              src={item.imageUrl}
+                              alt="card image"
+                              className="heroImg"
                             />
-                            <div className="bg-slate-200 px-6 py-4">
+                          </figure>
+                          <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+                            <header className="flex gap-4 mb-4">
+                              <a
+                                href="#"
+                                className="relative inline-flex items-center justify-center w-12 h-12 text-white rounded-full"
+                              >
+                                <img
+                                  src="https://i.pravatar.cc/48?img=24"
+                                  alt="user name"
+                                  title="user name"
+                                  width="48"
+                                  height="48"
+                                  className="max-w-full rounded-full"
+                                />
+                              </a>
+                              <div>
+                                <h3 className="text-xl font-medium text-slate-700">
+                                {item.heading}
+                                </h3>
+                                <p className="text-sm text-slate-400">Timing: {item.startTime} - {item.endTime}</p>
+                              </div>
                               {isCurrentDateWithinSchedule(
                                 item.startDate,
                                 item.endDate,
@@ -291,16 +303,14 @@ const SecondSection = () => {
                               ) : (
                                 <div></div>
                               )}
-                              <div className="font-bold text-2xl mb-2">
-                                {item.heading}
-                              </div>
-                              <h4 className="font-bold text-lg mb-2">
-                                Timing: {item.startTime} - {item.endTime}
-                              </h4>
-                              <p className="text-gray-700  text-lg">
-                                {item.shortDescription}
-                              </p>
-                              <AlertDialog>
+                            </header>
+                            <p>
+                              <b>{item.shortDescription}</b><br/>
+                              After a walk through history, there is nothing left to do but admire
+                              the hypnotizing landscapes that exist in every direction. From vast
+                              deserts to rainbow mountains, and everything in between.
+                            </p>
+                            <AlertDialog>
                                 <AlertDialogTrigger className="text-sm tracking-tighter">
                                   Know More
                                 </AlertDialogTrigger>
@@ -329,22 +339,22 @@ const SecondSection = () => {
                                   </AlertDialogFooter>
                                 </AlertDialogContent>
                               </AlertDialog>
-                            </div>
                           </div>
                         </div>
-                      </SwiperSlide>
-                    ))}
-                    <div className="slider-controler">
-                      <div className="swiper-button-prev slider-arrow">
-                        <img className="w-fit" src="/images/left.svg"></img>
-                      </div>
-                      <div className="swiper-button-next slider-arrow">
-                        <img className="w-fit" src="/images/right.svg"></img>
-                      </div>
-                      <div className="swiper-pagination"></div>
+                      
+                    </SwiperSlide>
+                  ))}
+                  <div className="slider-controler">
+                    <div className="swiper-button-prev slider-arrow">
+                      {/* <img className="w-fit" src="/left.svg"></img> */}
                     </div>
-                  </Swiper>
-                </div>
+                    <div className="swiper-button-next slider-arrow">
+                      {/* <img className="w-fit" src="/images/right.svg"></img> */}
+                    </div>
+
+                  </div>
+                </Swiper>
+
               </div>
             </div>
           )}
