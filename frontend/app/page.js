8b361a52e3ae549695929faf3cde679d450066b1
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-05-22 12:21:29 +0530

    error fixed

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -440,7 +440,7 @@ function Home() {
                 ></Image>
               </span>
               <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Conducted State's First Ever Tribal Youth Parliament.
+                Conducted State&apos;s First Ever Tribal Youth Parliament.
                 <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-orange-900 dark:text-orange-300 ms-3">
                   Latest
                 </span>
